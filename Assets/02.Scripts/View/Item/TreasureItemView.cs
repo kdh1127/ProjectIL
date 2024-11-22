2c9d14286026dcbe5b956c3d965c277d6b29bb1b
Jo_Chanhee <threerabbitgames@gmail.com>
2024-11-22 09:30:24 +0900

    보물 관련 UI 폴리싱

--- Assets/02.Scripts/View/Item/TreasureItemView.cs
+++ Assets/02.Scripts/View/Item/TreasureItemView.cs
@@ -11,21 +11,18 @@ public class TreasureItemView : MonoBehaviour
     public Image treasure_img;
     public TMP_Text name_txt;
     public TMP_Text level_txt;
-    public TMP_Text increaseType_txt;
-    public TMP_Text totalIncreasePer_txt;
+    public TMP_Text increase_txt;
 
     public void Init(/*Sprite sprite, */string name, string increaseType, BigInteger totalIncreasePer, int level)
     {
         //treasure_img.sprite = sprite;
         name_txt.text = name;
         level_txt.text = $"Lv.{level}(MAX.99999)";
-        increaseType_txt.text = increaseType;
-        totalIncreasePer_txt.text = $"{totalIncreasePer}%";
+        increase_txt.text = $"{increaseType} / {totalIncreasePer}";
     }
 
     public void LevelUpdate(int level, BigInteger totalIncreasePer)
     {
         level_txt.text = $"Lv.{level}(MAX.99999)";
-        totalIncreasePer_txt.text = $"{totalIncreasePer.ToAlphabetNumber()}%";
     }
 }

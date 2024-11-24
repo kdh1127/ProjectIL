b75d0af7a7aa6fdf2eab9f29c1da58c1b534f29e
Jo_Chanhee <threerabbitgames@gmail.com>
2024-11-25 06:52:51 +0900

    캐릭터 스킨 데미지 미적용 이슈 수정

--- Assets/02.Scripts/Model/CharacterModel.cs
+++ Assets/02.Scripts/Model/CharacterModel.cs
@@ -6,12 +6,12 @@ public class CharacterModel
     UserDataManager.CharacterData character => UserDataManager.Instance.characterData;
     public BigInteger BaseAttackDamage => (character.WeaponDamage
                                           + TreasureDamage
-                                          + TreasureExtraDamage) * character.SkinStatDictionary[ESkinIncreaseType.Damage];
+                                          + TreasureExtraDamage) + SkinDamage;
 
     /// <summary>
     /// 기본 공격력 * (기본 크리티컬 데미지 + (보물 크리티컬 증가 퍼센트값 / 100))
     /// </summary>
-    public BigInteger CriticalAttackDamage => (BaseAttackDamage * character.CriticalDamage) + TreasureCriticalDamage * character.SkinStatDictionary[ESkinIncreaseType.CriticalDamage];
+    public BigInteger CriticalAttackDamage => (BaseAttackDamage * character.CriticalDamage) + TreasureCriticalDamage + SkinCriticalDamage;
 
     /// <summary>
     /// 무기 데미지 + 보물로 인한 데미지 증가 퍼센트 / 100
@@ -21,6 +21,15 @@ public class CharacterModel
     public BigInteger TreasureExtraDamage => (character.WeaponDamage + TreasureDamage) * character.TreasureExtraDamage / 100;
 
     public BigInteger TreasureCriticalDamage => BaseAttackDamage * character.CriticalDamage * character.TreasureCriticalDamagePer / 100;
+
+    public BigInteger SkinDamage => (character.WeaponDamage
+                                          + TreasureDamage
+                                          + TreasureExtraDamage) * character.SkinStatDictionary[ESkinIncreaseType.Damage] / 100;
+
+    public BigInteger SkinCriticalDamage => (BaseAttackDamage * character.CriticalDamage + TreasureCriticalDamage)
+                                            * character.SkinStatDictionary[ESkinIncreaseType.CriticalDamage] / 100;
+
+    public BigInteger QuestGoldIncrease => (1 * character.TreasureQuestGoldPer) / 100 == 0? 1 : (1 * character.TreasureQuestGoldPer);
 
     public AttackInfo Attack()
     {
